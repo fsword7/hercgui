26168c11df7bd33addfd82b978a2b85266b5180d
dekeljacob <jacob@jacobdekel.com>
2010-07-03 07:01:51 +0000

    Added proper license to 3 files
    git-svn-id: http://svn.code.sf.net/p/hercstudio/code/trunk@41 aa892b33-f049-4de1-96b6-0e2ae40cb4a7

--- HerculesStudio/HercUtilities/SystemUtils.cpp
+++ HerculesStudio/HercUtilities/SystemUtils.cpp
@@ -1,11 +1,27 @@
 /*
- * SystemUtils.cpp
+ *  File:       SystemUtils.h
  *
+ *  Author:     Jacob Dekel
  *  Created on: Aug 24, 2009
- *      Author: yakovd
  *
- *  $Id: SystemUtils.cpp 34 2009-11-07 06:15:58Z jacob $
+ *  Copyright (c) 2009 Jacob Dekel
+ *  $Id:
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 
 #include "SystemUtils.h"
 
